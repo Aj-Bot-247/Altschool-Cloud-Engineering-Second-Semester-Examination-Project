40c943a0bca02abe4bff92dae55d18e07054403a
Ubuntu <ubuntu@ip-172-31-34-63.eu-west-1.compute.internal>
2025-06-14 00:16:49 +0000

    Initial commit for My AltSchool Cloud Engineering Exam Project

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading page');
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
